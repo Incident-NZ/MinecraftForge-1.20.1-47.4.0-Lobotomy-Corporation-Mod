33dda25c9838ed374851365e69cfddc0e30d7c43
Incident-NZ <haruki.t49@gmail.com>
2025-12-08 13:20:37 +0900

    Main Files

---
+++ src/main/java/net/lobotomy_corporation_mod/tabs/PMBlockTabs.java
@@ -0,0 +1,20 @@
+package net.lobotomy_corporation_mod.tabs;
+
+import net.lobotomy_corporation_mod.BlockInit;
+import net.lobotomy_corporation_mod.ItemInit;
+import net.minecraft.world.item.Item;
+
+public class PMBlockTabs {
+    public static final Item[] items = {
+            ItemInit.TASK_TABLET.get(),
+            BlockInit.BlockItems.E_GEN.get(),
+            BlockInit.BlockItems.ZAYIN_PE_BOX.get(),
+            BlockInit.BlockItems.TETH_PE_BOX.get(),
+            BlockInit.BlockItems.HE_PE_BOX.get(),
+            BlockInit.BlockItems.WAW_PE_BOX.get(),
+            BlockInit.BlockItems.ALEPH_PE_BOX.get(),
+            ItemInit.MAGIC_BULLET_AMMO.get(),
+            ItemInit.RIFLE_BULLET_AMMO.get(),
+            ItemInit.PISTOL_BULLET_AMMO.get(),
+    };
+}
