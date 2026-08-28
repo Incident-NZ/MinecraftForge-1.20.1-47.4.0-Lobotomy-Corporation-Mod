8043445bf5e1819e6f565a1559f3876063736ff7
Incident-NZ <haruki.t49@gmail.com>
2026-08-28 21:59:16 +0900

    Main Files

--- src/main/java/net/pm_equips/compat/Curios.java
+++ src/main/java/net/pm_equips/compat/Curios.java
@@ -1,8 +1,5 @@
 package net.pm_equips.compat;
 
 public class Curios {
-    public static String EGO_GIFT_SLOT = "ego_gift";
-    public static String BATTLE_GIFT_SLOT = "battle_gift";
     public static String CORE_PAGE_SLOT = "core_page";
-    public static String MARK_SLOT = "mark";
 }
